76f4b4f24f53a804f29b5f3181142c4985211180
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-09-09 16:36:25 -0700

    After reviewing both endpoints, fetched data on frist render

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,17 +7,57 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [characters, setCharacters] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  
   // ❗ Create effects to fetch the data and put it in state
+
+  useEffect(() => {
+    const fetchData = async () => {
+    try {
+      const [peopleResponse, planetsResponse] = await Promise.all([
+        fetch('http://localhost:9009/api/people'),
+        fetch('http://localhost:9009/api/planets'),
+      ]);
+
+      const peopleData = await peopleResponse.json();
+      const planetsData = await planetsResponse.json();
+
+  const peopleAndPlanets = peopleData.map(character => {
+    const homeworld = planetsData.find(planet => planet.id === character.homeworld);
+    return {
+      ...character,
+      homeworld: homeworld ? homeworld.name : 'Unknown',
+    }
+  });
+
+  setCharacters(peopleAndPlanets);
+} catch (error) {
+  console.log('Error fetching data', error);
+}
+};
+
+fetchData();
+}, []);
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      <ul>
+        {characters.map(character => (
+          <li key={character.id}>
+            <strong>{character.name}</strong> - Homeworld: {character.homeworld}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+
+export default App;
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
 if (typeof module !== 'undefined' && module.exports) module.exports = App
